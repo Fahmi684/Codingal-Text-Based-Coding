63d578c1234e054b00c78a4dda76f9f86633c4a2
Fahmi684 <fahmi.mridha@gmail.com>
2024-10-21 14:38:16 +0600

    Operator and data type

---
+++ Python/Python intro/main.py
@@ -0,0 +1,27 @@
+name = "Fahmi"
+age = 12
+is_student = True
+weight = 55
+
+print("Name is: ", name)
+print("Data type of name is: ", type(name))
+
+print("Age is: ", age)
+print("Data type of age is: ", type(age))
+
+print("Is student: ", is_student)
+print("Data type of is_student is: ", type(is_student))
+
+print("Weight is: ", weight)
+print("Data type of weight is: ", type(weight))
+
+
+print("/n After type Casting...")
+
+age = str(age)
+print(age)
+print("Data type of age is: ", type(age))
+
+weight = int(weight)
+print(weight)
+print("Data type of weight is: ", type(weight))
