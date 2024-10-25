2ba2a2910e645e68899e3321cf1b59331b172078
Fahmi684 <fahmi.mridha@gmail.com>
2024-10-25 11:36:18 +0600

    Operators

---
+++ Python/Operators/main.py
@@ -0,0 +1,21 @@
+num1 = 16
+num2 = 25
+
+print("Num1= ", num1)
+print("Num2= ", num2)
+print("Addition: ", num1+num2)
+print("Subtraction: ", num1-num2)
+print("Multiplication: ", num1*num2)
+print("Divition: ", num1/num2)
+print("Floor Division: ", num1//num2)
+print("Modulus Operation: ", num1%num2)
+print("Square: ", num1**2)
+print("Square root: ", num1**0.5)
+
+print("Equal? ", num1==num2)
+print("num1 greater? ", num1>num2)
+print("num2 greater? ", num1<num2)
+print("Not equal? ", num1!=num2)
+
+result = (num2/2)**3-(num1+num2)*2
+print("The result of the equation is: ", result)
