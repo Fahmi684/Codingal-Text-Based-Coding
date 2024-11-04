f9ab27f4a192cec7ffe6bdc2af9935aca42d49cc
Fahmi684 <fahmi.mridha@gmail.com>
2024-11-04 17:37:35 +0600

    Odd and even

---
+++ Python/Additional statement and list/main.py
@@ -0,0 +1,11 @@
+#Pogram 1#
+number = int(input("Enter number to check: "))
+
+if number>0:
+ print("This is a positive number")
+
+elif number<0:
+ print("This is a negative number")
+
+else:
+ print("This is a neutral number")
---
+++ Python/Odd and even/main.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter number to check: "))
+print("Number to be checked", number1)
+
+if number1%2==0:
+ print("This number is an even number")
+
+else:
+ print("This number is an odd number")
