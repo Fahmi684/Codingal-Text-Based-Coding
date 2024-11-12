ecd3a3c2c2388e2b92404f6439b50aa52925256f
Fahmi684 <fahmi.mridha@gmail.com>
2024-11-12 13:13:03 +0600

    Natural numbers

---
+++ Python/Natural numbers/main.py
@@ -0,0 +1,8 @@
+num = int(input("Enter number to check: "))
+
+if(num/1 == 0) :
+    print(num, "is a natural number")
+elif(num > 0) :
+    print(num, "is a natural number")
+else :
+    print(num, "is not a natural number")
