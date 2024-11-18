7b64127783cf6415edf54ded4b887a49e38c6c8b
Fahmi684 <fahmi.mridha@gmail.com>
2024-11-18 16:14:13 +0600

    Multiplication and Addition using function

---
+++ Python/Multiplication and Addition/main.py
@@ -0,0 +1,20 @@
+def multipy(a, b):
+    return a * b
+
+numa = int(input("Enter the first number for multiplication: "))
+numb = int(input("Enter the second number for multiplication: "))
+
+result_mul = multipy(numa, numb)
+print("The product is:", result_mul)
+
+
+def add(x, y):
+    return x + y
+
+numx = int(input("Enter the first number for addition: "))
+numy = int(input("Enter the second number for addition: "))
+
+result_add = add(numx, numy)
+print("The product is:", result_add)
+
+
