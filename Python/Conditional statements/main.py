112d7b7b1f16852624e962169736d4a3b7d8a571
Fahmi684 <fahmi.mridha@gmail.com>
2025-06-06 20:00:39 +0600

    Conditional statements

---
+++ Python/Conditional statements/main.py
@@ -0,0 +1,11 @@
+answer = input("When was the first use of the word 'quiz'?")
+if answer == "1781":
+    print("Correct!")
+else:
+    print(f"The ans is 1781, not {answer}")
+
+answer = input("What is the built in function to get interference from the user?")
+if answer == "Input":
+    print("Correct!")
+else:
+    print(f"The answer is 'input', not {answer}")
