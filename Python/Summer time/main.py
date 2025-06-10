42acd842e5e7178d955c096ff463de3f88d543da
Fahmi684 <fahmi.mridha@gmail.com>
2025-06-10 21:10:04 +0600

    Seasons

---
+++ Python/Summer time/main.py
@@ -0,0 +1,16 @@
+print("Summer = 1, Rainy = 2, Autumn = 3, Late Autumn = 4, Winter = 5 and Spring = 6")
+season = int(input("Enter a season based on the numbers given above: "))
+if(season==1):
+    print("Clothes: lightweight, breathable fabrics like cotton and linen, and opt for light-colored, loose-fitting clothing")
+elif(season==2):
+    print("Clothes: Light and Breathable Fabrics, Raincoats and Ponchos, Loose-fitting Silhouettes and Dark Jeans")
+elif(season==3):
+    print("light cotton kurtas, dhoti and shalwar for men, and stylish skirts for women")
+elif(season==4):
+    print("Long Sleeves, Jeans, corduroys, or comfortable pants, A lightweight jacket or cardigan and Breathable Fabrics")
+elif(season==5):
+    print("Jackets, Shawls, Overcoats, Sweaters, Pullovers and Cardigans, Long-sleeved T-shirts, Scarf and Gloves")
+elif(season==6):
+    print("lightweight and breathable fabrics like cotton and linen, and embrace floral prints and pastel colors")
+else:
+    print("Number not in the given text")
