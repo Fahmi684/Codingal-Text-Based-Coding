f0363d61a84aa04ee4d29df80ab844035f58ca98
Fahmi684 <fahmi.mridha@gmail.com>
2025-07-02 12:03:59 +0600

    Square

---
+++ Python/Square with turtle/main.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.Screen().bgcolor("lime")
+board = turtle.Turtle()
+
+angle = 90
+forward = 100
+
+board.forward(forward)
+board.right(angle)
+
+board.forward(forward)
+board.right(angle)
+
+board.forward(forward)
+board.right(angle)
+
+board.forward(forward)
+board.right(angle)
+
+turtle.done()
